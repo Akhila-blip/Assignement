05f721266573dab829960bbcbddd566b7bef12f2
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-05 17:13:24 -0500

    Added METHODS program

---
+++ METHODS.java
@@ -0,0 +1,26 @@
+METHODS:
+
+*.A Method Is A Way To Perform Some Task
+
+*.The Method In Java Is A Collection Of Instructions That Performs A Specific Task
+
+*.It Provides  The Reusability Of Code
+
+*.It Also Provides The Easy Modification And Readability Of Code
+
+*.The Most Imprtant Of Method In Java Is A main() Method
+
+*.The Method Declaration provides Information About Method Attributes,Such As Visibility,Retun type,And arguments
+
+*.The method Signature Includes The Method Name And Parameters List
+
+
+.There Are 2 Types Of Methods
+
+1)Predefined Method
+
+2)User -defined Method
+
+1)Predefined Method: Predefined Methods Are The Method That Is Already Defined In The Java Class Libraries,Such As print() Method Is Defined In The Java
+
+2)User-Defined Method:The Method Written By The User Or Programmer Is Known As A User=defined method
