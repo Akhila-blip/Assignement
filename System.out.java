ddb154f27e3aefeb098b7b9b3a22d8c731a09bb1
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-05 17:24:08 -0500

    Added System.out program

---
+++ System.out.java
@@ -0,0 +1,33 @@
+System.out.println In Java:
+
+*.In Java System.out.println() is Used to Print An Argument That Is Passed To It
+
+*.The Statement Can Be Broken Into 3 Parts
+
+1)System: It Is A Final Class Defined In The java.lang.package
+
+
+2 out: This Is An Instance Of PrintStream  Type,Which is a public And Static Member Field Of The System Class
+
+3)println(): 
+ *.As All Instances Of PrintStream class Have A   public method println()
+
+ *.This Is An Upgraded Version Of print()
+
+ *. It prints any argument passed to it and adds a new line to the output. 
+
+ *.We can assume that System.out represents the Standard Output Stream.
+
+  Syntax:
+         System.out.println(parameter)
+
+
+  Example:
+          public class A
+           {
+           public static void main(String[]args)
+           {
+             System.out.println("Welcome");
+               }
+                }
+
