380e27c01aaf23294de9c0f4f19134429708c0a2
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-05 17:09:50 -0500

    Added OBJECT program

---
+++ OBJECT.java
@@ -0,0 +1,20 @@
+OBJECT:
+
+*.An Object Is An Instance Of A Class
+
+*.An Object Has 3 Characterstics:
+ 1)State
+ 
+ 2)Behaviour
+
+ 3)Identity
+
+1)State:It Represents The Dta Of An Object
+
+2)Behaviour:It Represents The Behaviour Of An Object Such As Deposit,Withdraw, etc..
+
+3)Identity:An Object Identity Is Typically Implemented The Unique ID,The Value Of An Id Is Not Visible To The External User,It Is Used Internally By The Jvm.
+
+*.Syntax:
+
+  ClassName object=new ClassName();
